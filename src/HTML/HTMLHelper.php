4c0e86d745710d883453b8a3ad654097f87ff238
Bob Bloom <info@southlasalle.com>
2015-04-14 23:49:47 -0400

    HTML helper classes for CMS admin forms. #4

--- src/HTML/HTMLHelper.php
+++ src/HTML/HTMLHelper.php
@@ -36,6 +36,11 @@ use URL;
  */
 class HTMLHelper {
 
+
+    ///////////////////////////////////////////////////////////////////
+    ///////////////////    GENERAL HTML HELPERS    ////////////////////
+    ///////////////////////////////////////////////////////////////////
+
     /*
      * Convert yes/no to "check" or "remove" bootstrap buttons
      *
@@ -91,5 +96,292 @@ class HTMLHelper {
     }
 
 
- }
+    /*
+     * Format a db result set where an element is displayed on a separate line.
+     * Eg: A nice little list of categories for each post_id, with
+     *     each category on a separate line,
+     *
+     * @param  array  $results
+     * @return text
+     */
+    public static function listSingleCollectionElementOnSeparateRow($results, $element = "title")
+    {
+        if (count($results) <1 ) return '<span class="text-danger">n/a</span>';
+
+        $html = "";
+        $i = 1;
+        foreach ($results as $result)
+        {
+            if ($i == 1)
+            {
+                $html .= $result->$element;
+            } else {
+                $html .= "<br />".$result->$element;
+            }
+            $i++;
+        }
+
+        return $html;
+    }
+
+
+    ///////////////////////////////////////////////////////////////////
+    /////////////////      POST ADMIN HELPERS      ////////////////////
+    ///////////////////////////////////////////////////////////////////
+
+    /*
+     * Create a dropdown with multiple selects for the categories
+     *
+     * @param  collection  $categories  Laravel collection object of categories
+     * @return string
+     */
+    public static function postCategoryMultipleSelectCreate($categories)
+    {
+        // STEP 1: Initiatize the html select tag
+        $html = "";
+        $html .= '<select name="categories[]" id="categories" size="6" class="form-control" multiple>';
+
+        // STEP 2: Construct the <option></option> tags for ALL categories in the categories table
+        foreach ($categories as $category) {
+            $html .= '<option ';
+            $html .= 'value="';
+            $html .= $category->id;
+            $html .= '">';
+            $html .= $category->title;
+            $html .= '</option>"';
+        }
+        $html .= '</select>';
+
+        return $html;
+    }
+
+    /*
+     * Create a multiple select drop down for categories
+     * that have the existing categories for that post already selected
+     *
+     * @param  collection      $categories              All categories
+     * @param  int             $id                      Post ID
+     * @param  collection      $postAllCategoriesById   All catgories associated with this post
+     * @return string
+     */
+    public static function postCategoryMultipleSelectEdit($categories, $id, $postAllCategoriesById)
+    {
+        // STEP 1: Create an array of tag IDs that are currently attached to the post
+        $categories_attached_to_this_post = array();
+
+        foreach ($postAllCategoriesById as $category)
+        {
+            $categories_attached_to_this_post[] = $category->id;
+        }
+
+        // STEP 2: Initiatize the html select category
+        $html = "";
+        $html .=  '<select name="categories[]" id="tags" size="6" class="form-control" multiple>';
+
+        // STEP 3: Construct the <option></option> tags for ALL tags in the tags table
+        foreach ($categories as $category)
+        {
+            // If this tag is attached to the post, then SELECTED it
+            if ( in_array($category->id, $categories_attached_to_this_post) )
+            {
+                $selected = ' selected="selected" ';
+            } else {
+                $selected = "";
+            }
+            $html .= '<option ';
+            $html .= $selected;
+            $html .= 'value="';
+            $html .= $category->id;
+            $html .= '">';
+            $html .= $category->title;
+            $html .= '</option>"';
+        }
+        $html .= '</select>';
+        return $html;
+    }
+
+
+
+    /*
+     * Create a dropdown with multiple selects for the tags
+     *
+     * @param  collection  $tags  Laravel collection object of tags
+     * @return string
+     */
+    public static function postTagMultipleSelectCreate($tags)
+    {
+        // STEP 1: Initiatize the html select tag
+        $html = "";
+        $html .= '<select name="tags[]" id="tags" size="6" class="form-control" multiple>';
+
+        // STEP 2: Construct the <option></option> tags for ALL tags in the tags table
+        foreach ($tags as $tag) {
+            $html .= '<option ';
+            $html .= 'value="';
+            $html .= $tag->id;
+            $html .= '">';
+            $html .= $tag->title;
+            $html .= '</option>"';
+        }
+        $html .= '</select>';
+
+        return $html;
+    }
+
+    /*
+     * Create a multiple select drop down for tags
+     * that have the existing tags for that post already selected
+     *
+     * @param  collection      $tags              All tags
+     * @param  int             $id                Post ID
+     * @param  collection      $postAllTagsById   All tags associated with this post
+     * @return string
+     */
+    public static function postTagMultipleSelectEdit($tags, $id, $postAllTagsById)
+    {
+        // STEP 1: Create an array of tag IDs that are currently attached to the post
+        $tags_attached_to_this_post = array();
+
+        foreach ($postAllTagsById as $tag)
+        {
+            $tags_attached_to_this_post[] = $tag->id;
+        }
+
+        // STEP 2: Initiatize the html select tag
+        $html = "";        //foreach ( Post::find($id)->tags as $ttgg)
+        $html .=  '<select name="tags[]" id="tags" size="6" class="form-control" multiple>';
+
+        // STEP 3: Construct the <option></option> tags for ALL tags in the tags table
+        foreach ($tags as $tag)
+        {
+            // If this tag is attached to the post, then SELECTED it
+            if ( in_array($tag->id, $tags_attached_to_this_post) )
+            {
+                $selected = ' selected="selected" ';
+            } else {
+                $selected = "";
+            }
+            $html .= '<option ';
+            $html .= $selected;
+            $html .= 'value="';
+            $html .= $tag->id;
+            $html .= '">';
+            $html .= $tag->title;
+            $html .= '</option>"';
+        }
+        $html .= '</select>';
+        return $html;
+    }
+
+
+    ///////////////////////////////////////////////////////////////////
+    /////////////////    CATEGORY ADMIN HELPERS     ///////////////////
+    ///////////////////////////////////////////////////////////////////
+
+    /*
+     * Display the parent category
+     *
+     * @param  int         $parent_id
+     * @param  eloquent    $categoryRepository   The category repository
+     * @return string
+     */
+    public static function displayParentCategoryTitle($parent_id, $categoryRepository)
+    {
+        if ($parent_id == 0) return "";
+
+        return $categoryRepository->getFind($parent_id)->title;
+    }
+
+
+    /*
+     * Create a dropdown with a single select for the parent category
+     *
+     * @param  collection  $categories  Laravel collection object of category
+     * @return string
+     */
+    public static function categoryParentSingleSelectCreate($categories)
+    {
+        // STEP 1: Initiatize the html select tag
+        $html = "";
+        $html .= '<select name="parent_id" id="parent_id" size="6" class="form-control" >';
+
+        $html .= '<option ';
+        $html .= 'value="';
+        $html .= 0;
+        $html .= '">';
+        $html .= 'No Parent Category';
+        $html .= '</option>"';
+
+        // STEP 2: Construct the <option></option> categories for ALL categories in the categories table
+        foreach ($categories as $category) {
+            $html .= '<option ';
+            $html .= 'value="';
+            $html .= $category->id;
+            $html .= '">';
+            $html .= $category->title;
+            $html .= '</option>"';
+        }
+        $html .= '</select>';
+
+        return $html;
+    }
+
+
+    /*
+     * Create a multiple select drop down for tags
+     * that have the existing tags for that post already selected
+     *
+     * There is only one parent_id per category
+     *
+     * @param  collection       $categories        All categories
+     * @param  int              $parent id         category's parent id
+     * @param  int              $category id       category's id
+     * @return string
+     */
+    public static function categoryParentSingleSelectEdit($categories, $parent_id, $category_id)
+    {
+        // STEP 1: Initiatize the html select tag
+        $html = "";
+        $html .=  '<select name="parent_id" id="parent_id" size="6" class="form-control">';
+
+        $html .= '<option ';
+
+        if ( $parent_id == 0)  $html .= ' selected="selected" ';
+
+        $html .= 'value="';
+        $html .= 0;
+        $html .= '">';
+        $html .= 'No Parent Category';
+        $html .= '</option>"';
+
+        // STEP 2: Construct the <option></option> categories for ALL categories in the categories table
+        foreach ($categories as $category)
+        {
+            if ($category->id == $category_id) continue;
+
+
+            // If this tag is attached to the post, then SELECTED it
+            if ( $category->id == $parent_id )
+            {
+                $selected = ' selected="selected" ';
+            } else {
+                $selected = "";
+            }
+            $html .= '<option ';
+            $html .= $selected;
+            $html .= 'value="';
+            $html .= $category->id;
+            $html .= '">';
+            $html .= $category->title;
+            $html .= '</option>"';
+        }
+        $html .= '</select>';
+
+        return $html;
+    }
+
+
+
+
+}
 
